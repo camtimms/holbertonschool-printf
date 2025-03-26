76831df58e93ac9aa3ead4da83240f7d560ba4b2
camtimms <cam.g.timms@gmail.com>
2025-03-26 02:55:21 +0000

    added format_specifers.c worked on by Will and Cam

---
+++ format_specifiers.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_c - Print an ASCII chacater
+*
+* @n: ASCII value
+*
+* Description: Prints an ASCII chacater using
+* Return: Success (0)
+*/
+int print_c(int n)
+{
+	putchar(n);
+	return (0);
+}
+
+/**
+* print_s - Print a string
+*
+* @s: Input string
+*
+* Description: Prints a string (s)
+* Return: Success (0)
+*/
+int print_s(char *s)
+{
+	int i;
+
+	if (s == NULL)
+		s = "(null)";
+
+	for (i = 0; s[i] != '\0'; i++)
+		putchar(*s);
+
+	return (0);
+}
+
+
+/**
+* print_percent - Print a percent sign
+*
+* Description: Prints a percent sign (%)
+* Return: Success (0)
+*/
+int print_percent(void);
+{
+	putchar('%');
+	return (0);
+}
+
+/**
+* get_print_func - Function pointer to find the right function to use
+*
+* @s: Format specifier character from printf string argument
+*
+* Description: Goes through an array and returns the matching function to the
+* format specifer
+* Return: Success (0)
+*/
+int (*get_print_func(char *s))(void)
+{
+	format print_f[] = {
+	{"c", print_c},
+	{"s", print_s},
+	{"%", print_percent},
+	{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (print_f[i].c != NULL && s[1] == '\0')
+	{
+		if (*s == *print_f[i].c) /* c is for character */
+			return (print_f[i].f); /*  f is for function */
+		i++;
+	}
+
+	return (NULL);
+}
+
